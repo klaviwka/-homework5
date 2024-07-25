8bf49c0e1ae5e8751043a0c915e14f192f3b08af
klaviwka <klaviwka@mail.ru>
2024-07-25 16:40:47 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,6 @@
+immutable_var = 1, 'Spore', False
+print(immutable_var)
+#immutable_var [2] = "noName" нельзя поменять значения кортеджа, так как он не изменяем
+mutable_list = [1, 10, 'miss']
+mutable_list[2] = str("man")
+print(mutable_list)
